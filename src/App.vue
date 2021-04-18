98946ef4beb390fc2f3d1e94f54c3a70d5cd1dff
SerJLemast <maksiomrpp@gmail.com>
2021-04-18 12:45:14 +0300

    create work space

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <user-page />
+    </div>
+  </div>
+</template>
+
+<script>
+import UserPage from './components/UserPage'
+
+export default {
+  name: 'App',
+  components: {
+    UserPage
+  }
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import '@/assets/styles/main.scss';
+
+new Vue({
+  render: h => h(App),
+  el: '#app'
+})
